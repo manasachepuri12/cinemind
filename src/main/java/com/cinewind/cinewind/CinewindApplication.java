487158d5f1a3dfe97ad952c0ed792656f1115a34
Chepuri Manasa <manasa12.chepuri@gmail.com>
2026-05-30 00:31:59 +0530

    CHanged

--- src/main/java/com/cinewind/cinewind/CinewindApplication.java
+++ src/main/java/com/cinewind/cinewind/CinewindApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CinewindApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CinewindApplication.class, args);
 	}
-
 }
